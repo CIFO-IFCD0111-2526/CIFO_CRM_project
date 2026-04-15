907f05b422ca3e579c72afb21a7e1864b8c46451
LT2BS27482 <sandracalistote@hotmail.com>
2026-04-15 20:10:02 +0200

    TODO: faltan 2 middlewares que no se pueden linkar chore: afegits  middlware de json i urlencode , repassat config de session

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 
-//             por hora no hacen falta, aun  que nos vendrá bien.
-// const cookieParser = require("cookie-parser");
 const path = require("path");
 var session = require('express-session'); // per fer servir 
-require('dotenv').config({ path: `${path.join(__dirname,"..",".env")}` }); // per fer servir dades del .env //  habria que usar el path.join
-// require(`${path.join("MONTAR AQUI PARA LLEGAR A LOS MIDDLEWARE ")}`);  
+require('dotenv').config({ path: `${path.join(__dirname,"..",".env")}` }); // per fer servir dades del .env 
+
+
+// pendiente de que exista el middleware
+// require(`${path.join(__dirname,"src","middlewares", "auth.js")}`); 
+
 
 const express = require("express");
 const server = new express(); // creem el servidor
@@ -16,6 +18,9 @@ server.set('views', path.join(__dirname, 'views'));
 
 // servir estáticos desde public
 server.use(express.static(path.join(__dirname, "public")));
+server.use(express.json());
+server.use(express.urlencoded({ extended: true })); // extended ens permet enviar formularis
+
 
 // https://www.npmjs.com/package/express-session
 // info sobre el use de session , que s'haurà de llegir bé i 
@@ -23,20 +28,22 @@ server.use(express.static(path.join(__dirname, "public")));
 server.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false, // no guarda sessions anònimes
-    rolling: true, // para que se amplie la duracion de la cookie/sesson en cada petició
+    saveUninitialized: false,   // no guarda sessions anònimes
+    rolling: true,              // para que se amplie la duracion de la cookie/session en cada petició
     cookie: {
-            httpOnly: true, // si hem de treballar amb https , true, si no, false
-            secure:  process.env.NODE_ENV === "production" ,// entenent que a production farem servir httpS
-            maxAge: 6000000, // 100 minutos
-            
+            httpOnly: true,    // para que JS de navegador no pueda leer la cookie, podria donar problenmes de seguretat. 
+            secure:  process.env.NODE_ENV === "production" ,    // entenent que a production farem servir httpS
+            maxAge: 120 * 60 * 1000, // 120  minutos en ms ( 2h )            
         }
     }
 ))
-/*  la IA me idce que esto es necesario ? ? 
-// Necesario para procesar formularios POST
-server.use(express.urlencoded({ extended: true }));
-*/
+
+
+
+// pendiente de que exista 
+// server.use("/", require( `${path.join(__dirname,"routes","index.js")}` ));
+
+
 const PORT = process.env.PORT || 3000;
 // definim un port defecte o el del .env i comencem a escoltar 
 server.listen(PORT, () => console.log("Servidor activo en http://localhost:" + PORT));
