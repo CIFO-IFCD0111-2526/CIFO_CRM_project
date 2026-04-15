29b31db7c5519bf07a9bad51f89e74beaae99da4
LT2BS27482 <sandracalistote@hotmail.com>
2026-04-15 17:38:58 +0200

    feat: añadido  express-session, pendiente de configurar bien.

--- src/server.js
+++ src/server.js
@@ -17,9 +17,29 @@ const server = new express(); // creem el servidor
 // li diem que faci servir ejs com a viewer amb les vistes de ./views
 server.set('view engine', 'ejs');
 server.set('views', path.join(__dirname,'views')); 
+
+
 // servir estáticos desde public
 server.use(express.static(path.join(__dirname, "public")));
 
+// https://www.npmjs.com/package/express-session
+// info sobre el use de session , que s'haurà de configurar
+server.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: function(req) {
+    var match = req.url.match(/^\/([^/]+)/); // no sé para qué sirve
+    return {
+      path: match ? '/' + match[1] : '/',
+      httpOnly: true, // si hem de treballar amb https , true, si no, false
+      secure: req.secure || false,
+      maxAge: 60000,
+      rolling: true // para que se amplie la duracion de la cookie/sesson en cada petició
+
+    }
+  }
+}))
 
 
 
