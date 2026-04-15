29537767af151db0e5f26a1e7bb848dd1c99c27d
LT2BS27482 <sandracalistote@hotmail.com>
2026-04-15 17:49:31 +0200

    fix: añadido express-session y cambiado algunas rutas con path.join y creada la variable session

--- src/server.js
+++ src/server.js
@@ -1,47 +1,43 @@
 
 //             por hora no hacen falta, aun  que nos vendrá bien.
-// const path = require("path");
 // const cookieParser = require("cookie-parser");
-
-
-require('dotenv').config({ path: "../.env"}); // per fer servir dades del .env
-require("./mysql_conn.js"); 
+const path = require("path");
+var session = require('express-session'); // per fer servir 
+require('dotenv').config({ path: `${path.join("..",".env")}` }); // per fer servir dades del .env //  habria que usar el path.join
+// require(`${path.join("MONTAR AQUI PARA LLEGAR AL MIDDLEWARE ")}`);  
 
 const express = require("express");
-//console.log(process.env.PORT)
+const server = new express(); // creem el servidor
 
-
-
-// View Engine Setup
-const server = new express(); // creem el servidor 
 // li diem que faci servir ejs com a viewer amb les vistes de ./views
 server.set('view engine', 'ejs');
-server.set('views', path.join(__dirname,'views')); 
+server.set('views', path.join(__dirname, 'views'));
 
 
 // servir estáticos desde public
 server.use(express.static(path.join(__dirname, "public")));
 
 // https://www.npmjs.com/package/express-session
-// info sobre el use de session , que s'haurà de configurar
+// info sobre el use de session , que s'haurà de llegir bé i 
+// configurar
 server.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie: function(req) {
-    var match = req.url.match(/^\/([^/]+)/); // no sé para qué sirve
-    return {
-      path: match ? '/' + match[1] : '/',
-      httpOnly: true, // si hem de treballar amb https , true, si no, false
-      secure: req.secure || false,
-      maxAge: 60000,
-      rolling: true // para que se amplie la duracion de la cookie/sesson en cada petició
-
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: function (req) {
+        var match = req.url.match(/^\/([^/]+)/); // no sé para qué sirve
+        return {
+            path: match ? '/' + match[1] : '/',
+            httpOnly: true, // si hem de treballar amb https , true, si no, false
+            secure: req.secure || false,
+            maxAge: 60000,
+            rolling: true // para que se amplie la duracion de la cookie/sesson en cada petició
+        }
     }
-  }
 }))
 
 
 
 const PORT = process.env.PORT || 3000;
+// definim un port defecte o el del .env i comencem a escoltar 
 server.listen(PORT, () => console.log("Servidor activo en http://localhost:" + PORT));
