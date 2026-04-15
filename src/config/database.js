ffdef4e479fcf5f09516ff6a5f67584253f70398
Angel <angelpsan97@gmail.com>
2026-04-15 18:24:09 +0200

    fix: usuario no debia trabajarse en esta branch

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 const { Sequelize } = require("sequelize");
 
-const DB_NAME = process.env.DB_NAME || "CRM_CIFO";
-const DB_USER = process.env.DB_USER || "root";
-const DB_PASSWORD = process.env.DB_PASSWORD || "";
-const DB_HOST = process.env.DB_HOST || "localhost";
-const DB_PORT = process.env.DB_PORT || 3306;
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
