20f1fd76cbec00e583313af847c81587095a8bcc
josetheprol <josetheprol@gmail.com>
2026-04-16 16:48:20 +0200

    fix: corregidos '\routes\authRoutes.js' y '\routes\index.js'.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,5 +5,8 @@ const router = Router();
 
 // Rutas de autenticación
 
+router.post("/login", controller.loginForm);
 router.post("/login", controller.login);
 router.post("/logout", controller.logout);
+
+module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,1 @@
-const { Router } = require("express");
-const authRoutes = require("./authRoutes");
-
-const router = Router();
-
-router.use("/login", authRoutes);
+router.use("/", authRoutes);
