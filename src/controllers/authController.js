71db79de3c1adba2f049c43418bf180169595c13
josetheprol <josetheprol@gmail.com>
2026-04-16 18:17:21 +0200

    feat: loginForm, logout fix: login [controllers\authController.js] - Completada función 'login'. - Creadas funciones 'loginForm' y 'logout'

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,24 +1,65 @@
 const { Usuario } = require("..\models\Usuario.js");
 const bcrypt = require("bcrypt");
 
-// LOGIN
+// GET /login
+
+const loginForm = async (req, res) => {
+  // Se redirecciona a dashboard en caso de que el usuario esté logeado
+  if (req.session && req.session.usuario) {
+    return res.redirect("/dashboard");
+  }
+  // Se renderiza el ejs de login en caso de que no
+  res.render("login", {
+    titulo: "Login",
+    usuario: null,
+  });
+};
+
+// POST /login
+
 const login = async (req, res) => {
-    ({ loginEmail, loginPassword }) = req.body;
+    const ({ loginEmail, loginPassword }) = req.body;
+
     try {
         // Buscamos si existe el usuario en la tabla 'usuario' de la DB, y recogemos solamente su email y contraseña.
-        const userLogin = Usuario.findOne(loginEmail)({
+        const userLogin = Usuario.findOne({ where: { email : loginEmail, activo : true } })({
             attributes: ['password'],
         });
+        // Si no existe el usuario 
         if (userLogin === null) {
             res.status(400).json({ error: "El usuario no existe." });
         } else if (!bcrypt.compareSync(loginPassword, userLogin.password)) {
             res.status(400).json({ error: "Contraseña incorrecta." });
+        } else if (userLogin.activo === false) {
+            res.status(400).json({ error: "Usuario inactivo.". });
         } else {
             res.status(200).json({ message: "Login correcto!" });
         }
+
+        // Creamos la cookie de sesión en el req
+        req.session.usuario = {
+            id: userLogin.id,
+            nombre: userLogin.nombre,
+            apellidos: userLogin.apellidos,
+            email: userLogin.email,
+            nivel_acceso: userLogin.nivel_acceso,
+        };
+
+        // Respuesta OK
+
+        return res.json(200)({ ok: true, redirect: "/dashboard" });
+
     } catch (error) {
-        res.status(500).json({error: error.message});
+        res.status(500).json({ error: "Error de servidor." });
     }
 };
 
-// LOGOUT
+// POST /logout
+
+const logout = async (req, res) => {
+    req.session.destroy(() => {
+    res.json({ ok: true, redirect: "/login" });
+  });
+};
+
+module.exports = { loginForm, login, logout };
