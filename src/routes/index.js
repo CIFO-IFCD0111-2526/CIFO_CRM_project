ec1292e70044d91e0b743ac9fee09dd8ed7380b4
josetheprol <josetheprol@gmail.com>
2026-04-16 19:24:17 +0200

    fix: corregido index.js, posterior a clonar repo de nuevo por ¿bug?

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,9 @@
+const router = require("express").Router();
+
 router.use("/", authRoutes);
+
+router.get("/", (req, res) => {
+  res.send("CIFO CRM - Servidor OK");
+});
+
+module.exports = router;
