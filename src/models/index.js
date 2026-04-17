0e7f7478fb5759415d290782094cbb12f7a89a13
LT2BS27482 <sandracalistote@hotmail.com>
2026-04-17 18:29:03 +0200

    feat: afegides taules intermitges curso-alumno ( CursoAlumno - curso_alumno  ) uf-alumno  (AlumnoUf - "alumno_uf" )

--- src/models/index.js
+++ src/models/index.js
@@ -22,32 +22,62 @@ const Alumno = require("./Alumno");
 // TODO: definir relaciones cuando los modelos estén completos
 // -------------------------------------------------------
 
+
+
+//////////////////////////////////////////////////////////////////////TABLAS INTERMEDIAS ¿ A MOVER ?
+
+      // Aquest fa referència a l'objecte JS ( variable )
+const CursoAlumno = sequelize.define('curso_alumno', {   // És el nom que fa servir JS  ( string )
+  estat: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false, // false = pendent, true = aprovada/feta
+    allowNull: false
+    }
+}, {
+  tableName: 'curso_alumno',    // És el nom a la base de dades SQL ( string ) 
+})
+
+
+const AlumnoUf = sequelize.define('alumno_uf', {  
+  estat: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false, // false = pendent, true = aprovada/feta
+    allowNull: false
+    }
+}, {
+  tableName: 'alumno_uf',
+})
+
+/////////////////////////////////////////////////////////////////////TABLAS INTERMEDIAS ¿ A MOVER ?
+
+
 // los cursos tienen varias Uf, y las Uf pueden estar en mas de un curso
 Curso.belongsToMany(Uf, {through: 'curso_uf' });
 Uf.belongsToMany(Curso, {through: 'curso_uf' });
 
-//  los profesores pueden estar en varios cursos y algunos cursos pueden tener mas de un profe
+// los profesores pueden estar en varios cursos y algunos cursos pueden tener mas de un profe
 Profesor.belongsToMany(Curso, { through: "curso_profesor" });
 Curso.belongsToMany(Profesor, { through: "curso_profesor" });
 
-Alumno.belongsToMany(Curso,  { through: "curso_alumno" });
-Curso.belongsToMany(Alumno,  { through: "curso_alumno" });
-
+// Cursos tienen varios alumnos, los alumnos pueden estar en varios cursos, 
+// ( aunque no en dos cursos activos simultáneamente en teoria )
+Alumno.belongsToMany(Curso,  { through: CursoAlumno });
+Curso.belongsToMany(Alumno,  { through: CursoAlumno });
 
 // las UF estan en varios cursos y los cursos tiene varias Uf
-Uf.belongsToMany(Alumno,  { through: "alumno_uf" });
-Alumno.belongsToMany(Uf,  { through: "alumno_uf" });
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////// AQUI SE LIA XD
-
-
+Uf.belongsToMany(Alumno,  { through: AlumnoUf });
+Alumno.belongsToMany(Uf,  { through: AlumnoUf });
 
 const db = {
+  // taules SQL per nom JS
   sequelize,
   Usuario,
   Curso,
   Alumno,
-  Uf
+  Uf,
+  // tablas intermedias
+  CursoAlumno,
+  AlumnoUf
 };
 
 module.exports = db;
