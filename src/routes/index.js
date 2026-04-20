9f924012a31176fcd1f6b352e57301d35c5dac3e
TelesforoSol <telesforosolc@gmail.com>
2026-04-20 16:02:52 +0200

    feat/mount-routes en routes/index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,29 @@
 const router = require("express").Router();
 const authRoutes = require("./authRoutes");
+const alumnoRoutes = require("./alumnoRoutes");
+const cursoRoutes = require("./cursoRoutes");
+const profesorRoutes = require("./profesorRoutes");
+const ufRoutes = require("./ufRoutes");
+const usuarioRoutes = require("./usuarioRoutes");
 
 router.use("/", authRoutes);
+
+// Rutas con sus prefijos
+router.use("/alumnos", alumnoRoutes);
+router.use("/cursos", cursoRoutes);
+router.use("/ufs", ufRoutes);
+router.use("/profesores", profesorRoutes);
+router.use("/usuarios", usuarioRoutes)
 
 router.get("/", (req, res) => {
   if (req.session.usuario) return res.redirect("/dashboard");
   return res.redirect("/login");
 });
 
+// ruta dashboard
+router.get("/dashboard", (req, res) => {
+  if(!req.session.usuario) return res.redirect("/login");
+  res.render("dashboard", {usuario: req.session.usuario});
+});
 
 module.exports = router;
