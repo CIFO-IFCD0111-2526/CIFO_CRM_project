492afc1e7c3e4dbd25eb77715ac03c6738b1d624
TelesforoSol <telesforosolc@gmail.com>
2026-04-20 16:35:34 +0200

    ref: agregar authPage en ruta de dashboard
    chore: auth.js
    
    feat validaciones cliente registro & ruta authControllere
    
    feat: improve validation logic for login and registration forms
    
    Creada entrada en index.js, pendiente terminar.
    
    creat punt d'accñés endpoint /dashboard passant middleware i alguns params.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 const router = require("express").Router();
+const { authPage } = require("../middlewares/auth.js")
+
 const authRoutes = require("./authRoutes");
 const alumnoRoutes = require("./alumnoRoutes");
 const cursoRoutes = require("./cursoRoutes");
 const profesorRoutes = require("./profesorRoutes");
 const ufRoutes = require("./ufRoutes");
 const usuarioRoutes = require("./usuarioRoutes");
+
 
 router.use("/", authRoutes);
 
@@ -13,17 +16,28 @@ router.use("/alumnos", alumnoRoutes);
 router.use("/cursos", cursoRoutes);
 router.use("/ufs", ufRoutes);
 router.use("/profesores", profesorRoutes);
-router.use("/usuarios", usuarioRoutes)
+router.use("/usuarios", usuarioRoutes);
 
 router.get("/", (req, res) => {
   if (req.session.usuario) return res.redirect("/dashboard");
   return res.redirect("/login");
 });
-
 // ruta dashboard
-router.get("/dashboard", (req, res) => {
-  if(!req.session.usuario) return res.redirect("/login");
+router.get("/dashboard", authPage, (req, res) => {
   res.render("dashboard", {usuario: req.session.usuario});
+});
+
+
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard",
+    {
+      titulo: "Inici", /* ¿¿ tauler de control, Pagina principal ?? */
+      usuario: req.session.usuario , 
+      css: "dashboard.css",
+      js: "dashboard.js",
+
+    }
+  );
 });
 
 module.exports = router;
