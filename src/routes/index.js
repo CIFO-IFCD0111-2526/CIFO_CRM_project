0e6d64d1b49951860f463de4221e998f81336320
TelesforoSol <telesforosolc@gmail.com>
2026-04-20 16:02:52 +0200

    feat/mount-routes en routes/index

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,33 +4,28 @@ const { authPage } = require("../middlewares/auth.js");
 const authRoutes = require("./authRoutes");
 const alumnoRoutes = require("./alumnoRoutes");
 const cursoRoutes = require("./cursoRoutes");
-// const profesorRoutes = require("./profesorRoutes");
-// const ufRoutes = require("./ufRoutes");
-// const usuarioRoutes = require("./usuarioRoutes");
+const profesorRoutes = require("./profesorRoutes");
+const ufRoutes = require("./ufRoutes");
+const usuarioRoutes = require("./usuarioRoutes");
 
 router.use("/", authRoutes);
 
 // Rutas con sus prefijos
 router.use("/alumnos", alumnoRoutes);
 router.use("/cursos", cursoRoutes);
-// router.use("/ufs", ufRoutes);
-// router.use("/profesores", profesorRoutes);
-// router.use("/usuarios", usuarioRoutes);
+router.use("/ufs", ufRoutes);
+router.use("/profesores", profesorRoutes);
+router.use("/usuarios", usuarioRoutes)
 
 router.get("/", (req, res) => {
   if (req.session.usuario) return res.redirect("/dashboard");
   return res.redirect("/login");
 });
 
+// ruta dashboard
 router.get("/dashboard", (req, res) => {
-  res.render("dashboard",
-    {
-      titulo: "Inici", /* ¿¿ tauler de control, Pagina principal ?? */
-      usuario: req.session.usuario , 
-      css: "dashboard.css",
-      // js: "dashboard.js",
-    }
-  );
+  if(!req.session.usuario) return res.redirect("/login");
+  res.render("dashboard", {usuario: req.session.usuario});
 });
 
 module.exports = router;
