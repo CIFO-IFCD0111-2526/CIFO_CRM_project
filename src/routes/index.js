41885a1e98450b88904239872157bb69dceb6a60
TelesforoSol <telesforosolc@gmail.com>
2026-04-20 16:35:34 +0200

    ref: agregar authPage en ruta de dashboard

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 const router = require("express").Router();
+const { authPage } = require("../middlewares/auth.js")
+
 const authRoutes = require("./authRoutes");
 const alumnoRoutes = require("./alumnoRoutes");
 const cursoRoutes = require("./cursoRoutes");
 const profesorRoutes = require("./profesorRoutes");
 const ufRoutes = require("./ufRoutes");
 const usuarioRoutes = require("./usuarioRoutes");
+
 
 router.use("/", authRoutes);
 
@@ -13,7 +16,7 @@ router.use("/alumnos", alumnoRoutes);
 router.use("/cursos", cursoRoutes);
 router.use("/ufs", ufRoutes);
 router.use("/profesores", profesorRoutes);
-router.use("/usuarios", usuarioRoutes)
+router.use("/usuarios", usuarioRoutes);
 
 router.get("/", (req, res) => {
   if (req.session.usuario) return res.redirect("/dashboard");
@@ -21,8 +24,7 @@ router.get("/", (req, res) => {
 });
 
 // ruta dashboard
-router.get("/dashboard", (req, res) => {
-  if(!req.session.usuario) return res.redirect("/login");
+router.get("/dashboard", authPage, (req, res) => {
   res.render("dashboard", {usuario: req.session.usuario});
 });
 
