a44862de99611e5df22782fdab287b8586e4efcf
LT2BS27482 <sandracalistote@hotmail.com>
2026-04-20 16:24:09 +0200

    creat punt d'accñés endpoint /dashboard passant middleware i alguns params.

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,16 @@ router.get("/", (req, res) => {
   return res.redirect("/login");
 });
 
-router.get("/dashboard", (req, res) => { 
-  if ( !req.session.usuario) return res.redirect("/login");
-}) 
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard",
+    {
+      titulo: "Inici", /* ¿¿ tauler de control, Pagina principal ?? */
+      usuario: req.session.usuario , 
+      css: "dashboard.css",
+      js: "dashboard.js",
+
+    }
+  );
+});
+
 module.exports = router;
