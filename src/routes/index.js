8e2783a7eeed943a9ebb264c8e9a16630c27b24f
josetheprol <josetheprol@gmail.com>
2026-04-20 18:31:45 +0200

    feat: Creado dashboard.ejs, agregada bienvenida y div de stats.

--- src/routes/index.js
+++ src/routes/index.js
@@ -26,4 +26,16 @@ router.get("/", (req, res) => {
   if (req.session.usuario) return res.redirect("/dashboard");
   return res.redirect("/login");
 });
+
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard",
+    {
+      titulo: "Inici", /* ¿¿ tauler de control, Pagina principal ?? */
+      usuario: req.session.usuario , 
+      css: "dashboard.css",
+      // js: "dashboard.js",
+    }
+  );
+});
+
 module.exports = router;
