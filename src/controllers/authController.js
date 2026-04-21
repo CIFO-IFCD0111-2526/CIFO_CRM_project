f87b8670653b907f923f6ed7f68b3b765a07184f
Lyonc6 <carloscarranzae@gmail.com>
2026-04-21 19:52:07 +0200

    feat: Controller_post_password

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -146,9 +146,10 @@ const forgotPassword = async (req, res) => {
 
   try {
     // Comprobar email único
-    const existe = await Usuario.findOne({ where: { email, activo: true } });
+    const existe = await Usuario.findOne({ where: { email } });
     if (existe) {
       const nuevoPassword = generaContrasenaAleatoria();
+
       // Hashear nueva contraseña
       const hashedNuevoPassword = bcrypt.hashSync(nuevoPassword, 10);
 
