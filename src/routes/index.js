d6100b570c2efcd8a86c57d0f98a762e50dd9937
Angel <angelpsan97@gmail.com>
2026-04-21 19:53:03 +0200

    feat/verificacion del forgot password

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,6 @@ const authRoutes = require("./authRoutes");
 const dashboardRoutes = require("./dashboardRoutes");
 const alumnoRoutes = require("./alumnoRoutes");
 const cursoRoutes = require("./cursoRoutes");
-<<<<<<< HEAD
 const profesorRoutes = require("./profesorRoutes");
 const ufRoutes = require("./ufRoutes");
 const usuarioRoutes = require("./usuarioRoutes");
@@ -12,8 +11,6 @@ const dashboardRoutes = require("./dashboardRoutes");
 // const alumnoRoutes = require("./alumnoRoutes");
 // const cursoRoutes = require("./cursoRoutes");
 // const profesorRoutes = require("./profesorRoutes");
-=======
->>>>>>> 189c309 (ref: Updated professors view and added CSS/JS for styling)
 const ufRoutes = require("./ufRoutes");
 const profesorRoutes = require("./profesorRoutes");
 // const usuarioRoutes = require("./usuarioRoutes");
