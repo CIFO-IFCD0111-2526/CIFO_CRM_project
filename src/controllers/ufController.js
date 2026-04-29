76574ec2c22e02236b1657500dbe7244a31cc104
Capitankanalla <nanoxtechnologies@gmail.com>
2026-04-29 20:06:53 +0200

    chore: WIP CRUD UF-editar

--- src/controllers/ufController.js
+++ src/controllers/ufController.js
@@ -43,6 +43,40 @@ const renderNewUf = (req, res) => {
     });
 };
 
+// GET /ufs/:id/editar
+const getEditar = async (req, res) => {
+    const { id } = req.params;
+
+    const uf = await Uf.findByPk(id);
+
+    if (!uf) {
+        return res.redirect("/ufs");
+    }
+
+    res.render("uf-form", {
+        titulo: "Editar UF",
+        usuario: req.session.usuario,
+        paginaActual: "ufs",
+        css: "ufs.css",
+        js: "ufs.js",
+        uf,
+        errores: {},
+    });
+};
+
+// PUT /ufs/:id
+const putActualizar = async (req, res) => {
+    const { id } = req.params;
+    const { codigo, nombre, horas } = req.body;
+
+    const uf = await Uf.findByPk(id);
+    if (!uf) {
+        return res.status(404).json({ ok: false, redirect: "/ufs" });
+    }
+};
+
+const errores = {};
+
 // POST /ufs
 const createUf = async (req, res, next) => {
     const { codigo, nombre, horas } = req.body;
@@ -95,4 +129,4 @@ const deleteUf = async (req, res, next) => {
     }
 };
 
-module.exports = { getAll, getById, renderNewUf, createUf, deleteUf };
+module.exports = { getAll, getById,getNuevo, getEditar, putActualizar, postCrear, removeUf };
