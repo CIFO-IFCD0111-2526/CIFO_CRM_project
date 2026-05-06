f16bb19bd5c0a0f6b97e041c002db48ac3c25a66
armandolap <armando.lap@gmail.com>
2026-05-06 19:30:42 +0200

    fix: correct indentation and formatting in createRandomPassword function

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -163,11 +163,10 @@ const logout = async (req, res) => {
 // Función para generar contraseña aleatoria
 
 function createRandomPassword(length = 12) {
-const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*';
   let password = '';
   for (let i = 0; i < length; i++) {
-    const randomIndex = (crypto.randomInt(0, characters.length
-));
+    const randomIndex = crypto.randomInt(0, characters.length);
     password += characters[randomIndex];
   }
   return password;
