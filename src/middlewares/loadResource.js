94a6ac484ed83216626cb607296e6390d251114d
Lyonc6 <carloscarranzae@gmail.com>
2026-05-11 18:05:19 +0200

    refact: loadResource.js

--- src/middlewares/loadResource.js
+++ src/middlewares/loadResource.js
@@ -5,12 +5,15 @@ function loadResource(Modelo, options = {}) {
         notFoundMessage = `${Modelo.name} no trobat.`,
         propName = Modelo.name.toLowerCase(),
         include,
+        ownerField,
     } = options;
 
     return async (req, res, next) => {
         try {
             const recurso = await Modelo.findByPk(req.params[paramName], { include });
-            if (!recurso) {
+            const isNotFound = !recurso || (ownerField && recurso[ownerField] !== req.session.usuario?.id);
+
+            if (isNotFound) {
                 if (req.method === "GET") {
                     req.session.flash = {
                         type: "error",
@@ -21,6 +24,7 @@ function loadResource(Modelo, options = {}) {
                 }
                 return res.status(404).json({ ok: false, error: notFoundMessage });
             }
+
             req[propName] = recurso;
             next();
         } catch (error) {
