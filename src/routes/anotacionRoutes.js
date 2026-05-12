2c54043e4672e1f5c9d77c4e2dbce0d8d533a53a
armandolap <armando.lap@gmail.com>
2026-05-12 20:14:35 +0200

    feat: implement create annotation functionality with improved error handling and UI updates

--- src/routes/anotacionRoutes.js
+++ src/routes/anotacionRoutes.js
@@ -1,22 +1,11 @@
-/* const { Router } = require("express");
-const { authPage } = require("../middlewares/auth");
-
+const { Router } = require("express");
+const { authPage } = require("../middlewares/auth.js");
 const controller = require("../controllers/anotacionController");
 
 const router = Router();
 
 router.use(authPage);
 
-module.exports = router; */
-const router = require("express").Router();
-
-const controller = require("../controllers/anotacionController");
-const { authPage } = require("../middlewares/auth.js");
-
-// Protección de rutas
-router.use(authPage);
-
-// Crear anotación
 router.post("/", controller.create);
 
 module.exports = router;
