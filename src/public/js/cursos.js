cddab4aeef9975756e1af0e60b004aa91554794a
agusoc1968 <aortizc01@gmail.com>
2026-05-14 18:38:44 +0200

    Creado el filtro en cursos por año y funcionando. Se mantiene la estética global.

--- src/public/js/cursos.js
+++ src/public/js/cursos.js
@@ -147,7 +147,47 @@ document.addEventListener("DOMContentLoaded", () => {
     const dropdown = document.getElementById("dropdownResultados");
     if (input && dropdown) initBuscador(input, dropdown);
 });
+/* document.addEventListener("DOMContentLoaded", () => {
+    const filtroAnyCurso =
+        document.querySelector("#filtroAnyCurso");
+    if (!filtroAnyCurso) return;
+    filtroAnyCurso.addEventListener("change", () => {
+        const año = filtroAnyCurso.value;
+        const params =
+            new URLSearchParams(window.location.search);
+        if (año) {
+            params.set("año", año);
+        } else {
+            params.delete("año");
+        }
+        params.set("page", 1);
+        window.location.href =
+            `/cursos?${params.toString()}`;
+    });
 
+}); */
+
+document.addEventListener("DOMContentLoaded", () => {
+    const select = document.getElementById("filtroAnyCurso");
+
+    if (!select) return;
+
+    select.addEventListener("change", () => {
+        const year = select.value;
+
+        const url = new URL(window.location.href);
+
+        if (year) {
+            url.searchParams.set("año", year);
+        } else {
+            url.searchParams.delete("año");
+        }
+
+        url.searchParams.set("page", "1");
+
+        window.location.href = url.toString();
+    });
+});
 function initBuscador(input, dropdown) {
     let debounceTimer = null;
 
