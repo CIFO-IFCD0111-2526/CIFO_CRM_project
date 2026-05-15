f3c8bfd4420b63d8f770f26b85974be2707dd2ab
Capitankanalla <nanoxtechnologies@gmail.com>
2026-05-15 17:05:17 +0200

    feat: historic de cursos del alumne #160

--- src/routes/alumnoRoutes.js
+++ src/routes/alumnoRoutes.js
@@ -35,6 +35,11 @@ router.get("/:id",
     }),
     controller.getById
 );
+
+// router.get("/:id",
+//     loadResource(Alumno, { redirectTo: "/alumnos", include: [Curso] }),
+//     controller.getById
+// );
 router.put("/:id",
     loadResource(Alumno, { notFoundMessage: "L'alumne no existeix." }),
     controller.updateAlumno
