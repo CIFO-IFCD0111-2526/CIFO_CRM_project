4d83b84e80ce7a56e452c96af5459ad05c0cb530
Angel <angelpsan97@gmail.com>
2026-05-18 19:19:51 +0200

    crud: get,delete e includo download(que es otro get)

--- src/routes/documentoRoutes.js
+++ src/routes/documentoRoutes.js
@@ -8,5 +8,8 @@ const router = Router();
 router.use(authPage);
 
 router.post( "/:entidadTipo/:entidadId" , documentoController.uploadMiddleware , validateDocument , documentoController.uploadDocument );
+router.get( "/:entidadTipo/:entidadId",documentoController.getDocuments);
+router.get( "/:entidadTipo/:entidadId/:id/descarregar",documentoController.downloadDocument);
+router.delete("/:id",documentoController.deleteDocument);
 
 module.exports = router;
